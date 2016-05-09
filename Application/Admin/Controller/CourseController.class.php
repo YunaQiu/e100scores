27af053e47774a869c8190e0f09c33dd2a34ac17
qiuyu <qiuyu719@163.com>
2016-05-09 21:26:58 +0800

    新增科目的添加和修改功能

---
+++ Application/Admin/Controller/CourseController.class.php
@@ -0,0 +1,76 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+class CourseController extends AdminCommonController
+{
+	//新增科目页
+	public function add(){
+		$data['title'] = '新增科目';
+		$this->assign($data);
+		$this->display('Course/edit');
+	}
+
+	//修改科目页
+	public function edit(){
+		$alias = I('get.alias', false, '/^[0-9a-zA-Z]+$/');
+		$Course = D('Course');
+		if($alias === false){
+			$this->error('非法访问');
+		}else{
+			$id = $Course->getCourseId($alias);
+		}
+		$data = $Course->getCourseInfo($id);
+		if($data == null){
+			$this->error('找不到该科目');
+		}
+		//数据加工
+        $data['title'] = '编辑科目';
+        $this->assign($data);
+        $this->display();
+	}
+
+	//保存科目信息
+	public function save(){
+		//数据合法性检验及过滤
+		$data['id'] = I('post.id', 0, '/^\d+$/');
+		$data['name'] = I('post.name','');
+		$data['alias'] = I('post.alias', false, '/^[0-9a-zA-Z]+$/');
+		if ($data['name']=='' && $data['alias']){
+			$this->error('格式有误，请重新检查');
+			exit;
+		}
+		//数据有效性检验及转换处理
+		$Course = D('Course');
+		if ($data['id'] == 0){
+			unset($data['id']);
+			$type = 'add';
+		}else{
+			$type = 'update';
+		}
+		if ($Course->countAlias($id, $data['alias']) != 0){
+			$this->error('别名已被使用');
+			exit;
+		}
+		//写入数据库
+		if ($Course->saveRecord($data, $type)){
+			$this->success('操作成功', U('QuestionBank/index'));
+		}else{
+			$this->error('操作失败，请稍后重试');
+		}
+	}
+
+	// AJAX接口：验证别名可用性
+	public function checkAlias(){
+		$Course = D('Course');
+		$id = I('post.id', 0, '/^[0-9]+$/');
+		$alias = I('post.alias', '', '/^[0-9a-zA-Z]+$/');
+		if($Course->countAlias($id, $alias) == 0){
+			$data = 'success';
+		}else{
+			$data = 'error';
+		}
+		$this->ajaxReturn($data);
+	}
+}
+?>
--- Application/Admin/Model/CourseModel.class.php
+++ Application/Admin/Model/CourseModel.class.php
@@ -2,6 +2,46 @@
 namespace Admin\Model;
 use Think\Model;
 class CourseModel extends Model{
+	//字段验证规则
+	protected $_validate = array(
+			array('id', 'number', 'id不是数字'),
+			array('name', 'require', 'name不能为空'),
+			array('alias', '/^[0-9a-zA-Z]+$/', 'alias不合法'),
+		);
+
+	//获取科目列表，以array(alias=>name)的形式返回
+	public function getCourseList(){
+		$Course = M('Course');
+		$courseList = $Course->getField('alias, name');
+		return $courseList;
+	}
+
+	//返回一条科目记录的详细信息，为空时返回NULL
+	public function getCourseInfo($id){
+		$Course = M('Course');
+		$record = $Course->where('id="%d"', $id)->find();
+		return $record;
+	}
+
+	/**
+	 * 向数据表保存一条记录
+	 * @param data: 待保存的记录数组，键值包括: id(可选),name,alias
+	 * @param action: 操作类型，取值可为：add,update
+	 */
+	public function saveRecord($data, $action){
+		$Course = M('Course');
+		if ($Course->create($data)){
+			if ($action == 'add'){
+				$Course->add();
+			}else{
+				$Course->save();
+			}
+			return true;
+		}else{
+			return false;
+		}
+	}
+
 	//查找指定科目别名对应的科目id，查找不到时返回NULL
 	public function getCourseId($courseAlias){
 		$Course = M('Course');
@@ -9,11 +49,13 @@ class CourseModel extends Model{
 		return $result;
 	}
 
-	//获取科目列表，以array(alias=>name)的形式返回
-	public function getCourseList(){
+	//返回与指定科目重名的别名数量
+	public function countAlias($id, $alias){
 		$Course = M('Course');
-		$courseList = $Course->getField('alias, name');
-		return $courseList;
+		$map['id'] = array('neq', $id);
+		$map['alias'] = array('eq', $alias);
+		$total = $Course->where($map)->count();
+		return $total;
 	}
 }
 ?>
