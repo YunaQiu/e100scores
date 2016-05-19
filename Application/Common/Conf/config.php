9c39d91c33807abafa008290b917856a9f41ff10
qiuyu <qiuyu719@163.com>
2016-05-19 17:32:54 +0800

    去掉路由中的index.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,6 +2,8 @@
 return array(
 	//'配置项'=>'配置值'
     'LOAD_EXT_CONFIG' => 'db',
+    'URL_MODEL' => '2',
+    'URL_HTML_SUFFIX' => '',
     
     'DEFAULT_FILTER' => 'trim,htmlspecialchars',
     define('ALIAS_FORMAT', '/^[0-9a-zA-Z_]+$/'),
